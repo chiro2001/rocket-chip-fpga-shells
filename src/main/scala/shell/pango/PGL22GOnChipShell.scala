ef0571f5fd4216d155554f01369592c683dee4f9
Chiro Liang <Chiro2001@163.com>
2022-10-30 14:36:51 +0800

    qspi

--- src/main/scala/shell/pango/PGL22GOnChipShell.scala
+++ src/main/scala/shell/pango/PGL22GOnChipShell.scala
@@ -5,7 +5,8 @@ import chisel3._
 import sifive.fpgashells.shell.{JTAGDebugOverlayKey, JTAGDebugShellInput, SPIFlashOverlayKey, SPIFlashShellInput}
 import sifive.fpgashells.shell.pango.{JTAGDebugPGL22GShellPlacer, PGL22GPerfShell, PGL22GShellBasicOverlays, SPIFlashPGL22GShellPlacer}
 
-abstract class PGL22GOnChipShell(implicit p: Parameters) extends PGL22GShellBasicOverlays {
+abstract class PGL22GOnChipShell(implicit p: Parameters)
+  extends PGL22GShellBasicOverlays {
   val jtag = Overlay(JTAGDebugOverlayKey, new JTAGDebugPGL22GShellPlacer(this, JTAGDebugShellInput()))
   val spi_flash = Overlay(SPIFlashOverlayKey, new SPIFlashPGL22GShellPlacer(this, SPIFlashShellInput()))
 }
