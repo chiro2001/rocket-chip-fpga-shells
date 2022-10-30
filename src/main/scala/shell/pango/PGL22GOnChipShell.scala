4166be347dc39832ce85d87cb7375e0413fc4280
Chiro Liang <Chiro2001@163.com>
2022-10-30 10:21:20 +0800

    onchip: add peripherals

--- src/main/scala/shell/pango/PGL22GOnChipShell.scala
+++ src/main/scala/shell/pango/PGL22GOnChipShell.scala
@@ -2,9 +2,10 @@ package shell.pango
 
 import chipsalliance.rocketchip.config.Parameters
 import chisel3._
-import sifive.fpgashells.shell.{JTAGDebugOverlayKey, JTAGDebugShellInput}
-import sifive.fpgashells.shell.pango.{JTAGDebugPGL22GShellPlacer, PGL22GPerfShell, PGL22GShellBasicOverlays}
+import sifive.fpgashells.shell.{JTAGDebugOverlayKey, JTAGDebugShellInput, SPIFlashOverlayKey, SPIFlashShellInput}
+import sifive.fpgashells.shell.pango.{JTAGDebugPGL22GShellPlacer, PGL22GPerfShell, PGL22GShellBasicOverlays, SPIFlashPGL22GShellPlacer}
 
 abstract class PGL22GOnChipShell(implicit p: Parameters) extends PGL22GShellBasicOverlays {
   val jtag = Overlay(JTAGDebugOverlayKey, new JTAGDebugPGL22GShellPlacer(this, JTAGDebugShellInput()))
+  val spi_flash = Overlay(SPIFlashOverlayKey, new SPIFlashPGL22GShellPlacer(this, SPIFlashShellInput()))
 }
