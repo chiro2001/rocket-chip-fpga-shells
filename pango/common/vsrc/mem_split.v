415d2a72043d630af4f30ce0d29f5bd0604ff8ca
Chiro Liang <Chiro2001@163.com>
2022-11-09 22:26:37 +0800

    update

--- pango/common/vsrc/mem_split.v
+++ pango/common/vsrc/mem_split.v
@@ -84,18 +84,6 @@ split_drm_8x1024 split_drm_8x1024_inst_1 (
 );
 endmodule
 
-module split_drm_8x64(
-  input  [5:0]  RW0_addr,
-  input         RW0_clk,
-  input  [7:0]  RW0_wdata,
-  output [7:0]  RW0_rdata,
-  input         RW0_en,
-  input         RW0_wmode,
-  input         RW0_wmask
-);
-drm_8x64 drm_data_9k (`DRMM);
-endmodule
-
 module split_drm_32x1024_nomask(
   input  [9:0]    RW0_addr,
   input           RW0_clk,
@@ -321,115 +309,6 @@ module split_drm_128x1024(
   assign mem_0_3_RW0_addr = RW0_addr;
   assign mem_0_3_RW0_clk = RW0_clk;
   assign mem_0_3_RW0_wdata = RW0_wdata[127:96];
-  assign mem_0_3_RW0_en = RW0_en;
-  assign mem_0_3_RW0_wmode = RW0_wmode;
-  assign mem_0_3_RW0_wmask = RW0_wmask;
-endmodule
-
-module split_drm_32x64(
-  input  [5:0]    RW0_addr,
-  input           RW0_clk,
-  input  [31:0]   RW0_wdata,
-  output [31:0]   RW0_rdata,
-  input           RW0_en,
-  input           RW0_wmode,
-  input           RW0_wmask
-);
-  wire [5:0] mem_0_0_RW0_addr;
-  wire  mem_0_0_RW0_clk;
-  wire [7:0] mem_0_0_RW0_wdata;
-  wire [7:0] mem_0_0_RW0_rdata;
-  wire  mem_0_0_RW0_en;
-  wire  mem_0_0_RW0_wmode;
-  wire  mem_0_0_RW0_wmask;
-  wire [5:0] mem_0_1_RW0_addr;
-  wire  mem_0_1_RW0_clk;
-  wire [7:0] mem_0_1_RW0_wdata;
-  wire [7:0] mem_0_1_RW0_rdata;
-  wire  mem_0_1_RW0_en;
-  wire  mem_0_1_RW0_wmode;
-  wire  mem_0_1_RW0_wmask;
-  wire [5:0] mem_0_2_RW0_addr;
-  wire  mem_0_2_RW0_clk;
-  wire [7:0] mem_0_2_RW0_wdata;
-  wire [7:0] mem_0_2_RW0_rdata;
-  wire  mem_0_2_RW0_en;
-  wire  mem_0_2_RW0_wmode;
-  wire  mem_0_2_RW0_wmask;
-  wire [5:0] mem_0_3_RW0_addr;
-  wire  mem_0_3_RW0_clk;
-  wire [7:0] mem_0_3_RW0_wdata;
-  wire [7:0] mem_0_3_RW0_rdata;
-  wire  mem_0_3_RW0_en;
-  wire  mem_0_3_RW0_wmode;
-  wire  mem_0_3_RW0_wmask;
-  wire [7:0] RW0_rdata_0_0 = mem_0_0_RW0_rdata;
-  wire [7:0] RW0_rdata_0_1 = mem_0_1_RW0_rdata;
-  wire [7:0] RW0_rdata_0_2 = mem_0_2_RW0_rdata;
-  wire [7:0] RW0_rdata_0_3 = mem_0_3_RW0_rdata;
-  wire [15:0] _GEN_0 = {RW0_rdata_0_1,RW0_rdata_0_0};
-  wire [23:0] _GEN_1 = {RW0_rdata_0_2,RW0_rdata_0_1,RW0_rdata_0_0};
-  wire [31:0] RW0_rdata_0 = {RW0_rdata_0_3,RW0_rdata_0_2,RW0_rdata_0_1,RW0_rdata_0_0};
-  wire [15:0] _GEN_2 = {RW0_rdata_0_1,RW0_rdata_0_0};
-  wire [23:0] _GEN_3 = {RW0_rdata_0_2,RW0_rdata_0_1,RW0_rdata_0_0};
-  split_drm_8x64 mem_0_0 (
-    .RW0_addr(mem_0_0_RW0_addr),
-    .RW0_clk(mem_0_0_RW0_clk),
-    .RW0_wdata(mem_0_0_RW0_wdata),
-    .RW0_rdata(mem_0_0_RW0_rdata),
-    .RW0_en(mem_0_0_RW0_en),
-    .RW0_wmode(mem_0_0_RW0_wmode),
-    .RW0_wmask(mem_0_0_RW0_wmask)
-  );
-  split_drm_8x64 mem_0_1 (
-    .RW0_addr(mem_0_1_RW0_addr),
-    .RW0_clk(mem_0_1_RW0_clk),
-    .RW0_wdata(mem_0_1_RW0_wdata),
-    .RW0_rdata(mem_0_1_RW0_rdata),
-    .RW0_en(mem_0_1_RW0_en),
-    .RW0_wmode(mem_0_1_RW0_wmode),
-    .RW0_wmask(mem_0_1_RW0_wmask)
-  );
-  split_drm_8x64 mem_0_2 (
-    .RW0_addr(mem_0_2_RW0_addr),
-    .RW0_clk(mem_0_2_RW0_clk),
-    .RW0_wdata(mem_0_2_RW0_wdata),
-    .RW0_rdata(mem_0_2_RW0_rdata),
-    .RW0_en(mem_0_2_RW0_en),
-    .RW0_wmode(mem_0_2_RW0_wmode),
-    .RW0_wmask(mem_0_2_RW0_wmask)
-  );
-  split_drm_8x64 mem_0_3 (
-    .RW0_addr(mem_0_3_RW0_addr),
-    .RW0_clk(mem_0_3_RW0_clk),
-    .RW0_wdata(mem_0_3_RW0_wdata),
-    .RW0_rdata(mem_0_3_RW0_rdata),
-    .RW0_en(mem_0_3_RW0_en),
-    .RW0_wmode(mem_0_3_RW0_wmode),
-    .RW0_wmask(mem_0_3_RW0_wmask)
-  );
-  assign RW0_rdata = {RW0_rdata_0_3,_GEN_1};
-  assign mem_0_0_RW0_addr = RW0_addr;
-  assign mem_0_0_RW0_clk = RW0_clk;
-  assign mem_0_0_RW0_wdata = RW0_wdata[7:0];
-  assign mem_0_0_RW0_en = RW0_en;
-  assign mem_0_0_RW0_wmode = RW0_wmode;
-  assign mem_0_0_RW0_wmask = RW0_wmask;
-  assign mem_0_1_RW0_addr = RW0_addr;
-  assign mem_0_1_RW0_clk = RW0_clk;
-  assign mem_0_1_RW0_wdata = RW0_wdata[15:8];
-  assign mem_0_1_RW0_en = RW0_en;
-  assign mem_0_1_RW0_wmode = RW0_wmode;
-  assign mem_0_1_RW0_wmask = RW0_wmask;
-  assign mem_0_2_RW0_addr = RW0_addr;
-  assign mem_0_2_RW0_clk = RW0_clk;
-  assign mem_0_2_RW0_wdata = RW0_wdata[23:16];
-  assign mem_0_2_RW0_en = RW0_en;
-  assign mem_0_2_RW0_wmode = RW0_wmode;
-  assign mem_0_2_RW0_wmask = RW0_wmask;
-  assign mem_0_3_RW0_addr = RW0_addr;
-  assign mem_0_3_RW0_clk = RW0_clk;
-  assign mem_0_3_RW0_wdata = RW0_wdata[31:24];
   assign mem_0_3_RW0_en = RW0_en;
   assign mem_0_3_RW0_wmode = RW0_wmode;
   assign mem_0_3_RW0_wmask = RW0_wmask;
